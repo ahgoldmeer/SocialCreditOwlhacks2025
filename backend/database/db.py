988a2314d7b365675da10d3b2de986ddb7a1b93e
Wyatt Zantua <tuo18351@temple.edu>
2025-09-27 13:32:12 -0400

    added env variables for mongodb auth

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,8 +1,9 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os
 
 def test():
-    uri = "mongodb+srv://wyatt_db_admin:y4Dabv1SNdNuUrYw@creditcluster.weqoje8.mongodb.net/?retryWrites=true&w=majority&appName=CreditCluster"
+    uri = os.environ.get('MONGODB_URI')
 
     # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
